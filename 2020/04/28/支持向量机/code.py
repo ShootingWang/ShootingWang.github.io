d4da4d8519a134388154ab81513d12accd1a0f52
shootingwang <stwang95@163.com>
2020-04-28 11:48:22 +0800

    Site updated: 2020-04-28 11:48:21

--- 2020/04/28/支持向量机/code.py
+++ 2020/04/28/支持向量机/code.py
@@ -32,7 +32,7 @@ for m, b in [(1, 0.65), (0.5, 1.6), (-0.2, 2.9)]:
 plt.xlim(-1, 3.5)
 
 xfit = np.linspace(-1, 3.5)
-plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
 
 for m, b, d in [(1, 0.65, 0.33), (0.5, 1.6, 0.55), (-0.2, 2.9, 0.2)]:
     yfit = m * xfit + b
@@ -40,3 +40,37 @@ for m, b, d in [(1, 0.65, 0.33), (0.5, 1.6, 0.55), (-0.2, 2.9, 0.2)]:
     plt.fill_between(xfit, yfit - d, yfit + d, edgecolor='none', color='#AAAAAA', alpha=0.4)
 
 plt.xlim(-1, 3.5)
+
+import numpy as np
+from sklearn.svm import SVC
+## Support Vector Classifier
+model = SVC(kernel='linear', C=1E10)
+model.fit(X, y)
+
+def plot_svc_decision_function(model, ax=None, plot_support=True):
+    """
+    画二维SVC的决策函数
+    """
+    if ax is None:
+        ax = plt.gca()  ## 获得当前的Axes对象ax
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    
+    x = np.linspace(xlim[0], xlim[1], 30)
+    y = np.linspace(ylim[0], ylim[1], 30)
+    Y, X = np.meshgrid(y, x)   ## 创建网格
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+    
+    ## 画决策直线和边界
+    ax.contour(X, Y, P, color='k', levels=[-1, 0, 1], alpha=0.5, linestyles=['--', '-', '--'])
+    ## 画支持向量
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0],
+                   model.support_vectors_[:, 1],
+                   s=300, linewidth=1, facecolors='none')
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(model)
